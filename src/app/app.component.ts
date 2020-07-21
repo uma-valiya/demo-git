a5e73757608d8924c852fd71da157d0dd11bf297
guest user <68589173+uma-demo@users.noreply.github.com>
2020-07-21 02:52:08 -0700

    Create another conflicts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,6 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.analytics.trackPageViews();
     this.seoService.trackCanonicalChanges();
-    console.log("demo for merge conflicts");
     this.addConsoleLog();
   }
   addConsoleLog() {
