81067004cef1477ca611ba6f097247b1be800eec
Uma Valiya <uma.valiya2@gmail.com>
2020-07-21 02:53:03 -0700

    Create another conflicts - 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.analytics.trackPageViews();
     this.seoService.trackCanonicalChanges();
-    console.log("demo for merge conflicts");
+    console.log("demo for merge conflicts - updated");
     this.addConsoleLog();
   }
   addConsoleLog() {
