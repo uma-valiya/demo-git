fabb4f9cc28795f91bfa88e2f193de31110963d3
Uma Valiya <uma.valiya2@gmail.com>
2020-07-25 01:29:24 -0700

    Remove console log function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,9 +19,5 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.analytics.trackPageViews();
     this.seoService.trackCanonicalChanges();
-    this.addConsoleLog();
-  }
-  addConsoleLog() {
-    console.log("add console");
   }
 }
